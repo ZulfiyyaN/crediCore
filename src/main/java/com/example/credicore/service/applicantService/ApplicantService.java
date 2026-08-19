ad5909a91faf96eb684c4a3616d05dc1143cb043
Zulfiyya Naghiyeva <naghiyevazulfiyya@gmail.com>
2026-08-20 00:18:12 +0400

    created createApplicant methods at the Applicant Services

--- src/main/java/com/example/credicore/service/applicantService/ApplicantService.java
+++ src/main/java/com/example/credicore/service/applicantService/ApplicantService.java
@@ -1,6 +1,11 @@
 package com.example.credicore.service.applicantService;
 
+import com.example.credicore.model.request.ApplicantRequest;
+
 public interface ApplicantService {
+
+
+    public void createApplicant (ApplicantRequest request);
 
 
 }
--- src/main/java/com/example/credicore/service/applicantService/ApplicantServiceImpl.java
+++ src/main/java/com/example/credicore/service/applicantService/ApplicantServiceImpl.java
@@ -1,4 +1,22 @@
 package com.example.credicore.service.applicantService;
 
+import com.example.credicore.mapper.ApplicantMapper;
+import com.example.credicore.model.entity.ApplicantEntity;
+import com.example.credicore.model.request.ApplicantRequest;
+import com.example.credicore.repository.ApplicantRepository;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class ApplicantServiceImpl implements ApplicantService {
+
+    ApplicantMapper applicantMapper;
+    ApplicantRepository applicantRepository;
+
+
+    @Override
+    public void createApplicant(ApplicantRequest request) {
+        ApplicantEntity applicantEntity = applicantMapper.fromRequestToApplicant(request);
+        applicantRepository.save(applicantEntity);
+        log.info("Applicant created");
+    }
 }
