8ea715b54a5f5f5199bf4d4915d86a5a228ce8ba
Zulfiyya Naghiyeva <naghiyevazulfiyya@gmail.com>
2026-08-22 17:35:33 +0400

    feat: added eligiblities for ApplicantServiceImpl

--- src/main/java/com/example/credicore/service/applicantService/ApplicantServiceImpl.java
+++ src/main/java/com/example/credicore/service/applicantService/ApplicantServiceImpl.java
@@ -1,16 +1,23 @@
 package com.example.credicore.service.applicantService;
 
+import com.example.credicore.exception.ApplicantNotEligibleException;
 import com.example.credicore.mapper.ApplicantMapper;
 import com.example.credicore.model.entity.ApplicantEntity;
 import com.example.credicore.model.request.ApplicantRequest;
 import com.example.credicore.repository.ApplicantRepository;
+import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.Period;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class ApplicantServiceImpl implements ApplicantService {
 
     ApplicantMapper applicantMapper;
@@ -19,6 +26,17 @@ public class ApplicantServiceImpl implements ApplicantService {
 
     @Override
     public void createApplicant(ApplicantRequest request) {
+        LocalDate birthDate = request.getBirthDate();
+        int age = Period.between(birthDate, LocalDate.now()).getYears();
+
+        if (age < 18) {
+            throw new ApplicantNotEligibleException("Applicant must be at least 18 years old");
+        }
+
+        if (request.getMonthlyIncome() <= request.getTotalMonthlyDebt()) {
+            throw new ApplicantNotEligibleException("Applicant income is not enough");
+        }
+
         ApplicantEntity applicantEntity = applicantMapper.fromRequestToApplicant(request);
         applicantRepository.save(applicantEntity);
         log.info("Applicant created");
