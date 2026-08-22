884fba59f2d8f174fc97dcd1873009b496aa7f9b
Zulfiyya Naghiyeva <naghiyevazulfiyya@gmail.com>
2026-08-22 12:14:50 +0400

    feat: add service annotation and constructor injection

--- src/main/java/com/example/credicore/service/applicantService/ApplicantServiceImpl.java
+++ src/main/java/com/example/credicore/service/applicantService/ApplicantServiceImpl.java
@@ -4,9 +4,13 @@ import com.example.credicore.mapper.ApplicantMapper;
 import com.example.credicore.model.entity.ApplicantEntity;
 import com.example.credicore.model.request.ApplicantRequest;
 import com.example.credicore.repository.ApplicantRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
+@Service
 @Slf4j
+@RequiredArgsConstructor
 public class ApplicantServiceImpl implements ApplicantService {
 
     ApplicantMapper applicantMapper;
@@ -18,5 +22,6 @@ public class ApplicantServiceImpl implements ApplicantService {
         ApplicantEntity applicantEntity = applicantMapper.fromRequestToApplicant(request);
         applicantRepository.save(applicantEntity);
         log.info("Applicant created");
+
     }
 }
